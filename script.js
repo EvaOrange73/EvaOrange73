103ae9ae0593692220c757a2568e6dcb09496ab3
EvaOrange73 <63116721+EvaOrange73@users.noreply.github.com>
2020-04-04 23:42:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let v = 1;
-let alpha = 0;
 let ball = document.getElementById("ball");
 
 document.addEventListener('mousemove', moveBall, false);
@@ -18,20 +16,25 @@ function moveBall(e){
     let dx = mx - bx - 50;
     let dy = my - by - 50;
 
-    alpha = Math.atan(dy/dx);
+    let alpha = Math.atan(dy/dx);
     if (dx < 0){
         alpha = Math.PI + alpha;
     }
 
-    if (ballCoords.right >= bodyCoords.right || ballCoords.left <= 0) {
-        alpha = Math.PI - alpha;
-    } else if(ballCoords.bottom >= bodyCoords.bottom || ballCoords.top <= 0){
-        alpha = -alpha;
+    let R = Math.sqrt(dx**2 + dy**2)
+    let v = 3;
+    if (R <= 600){
+        v -= R / 200;
     }
 
     bx += v * Math.cos(alpha);
     by += v * Math.sin(alpha);
 
+    if (ballCoords.right >= bodyCoords.right || ballCoords.left <= 0) {
+        bx = 1;
+    } else if(ballCoords.bottom >= bodyCoords.bottom || ballCoords.top <= 0){
+        by = 1;
+    }
     ball.style.left = `${bx}px`;
     ball.style.top = `${by}px`;
 
