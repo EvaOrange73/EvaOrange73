6c8928b1529329f670c6d8b167b2b9a0df27717c
EvaOrange73 <63116721+EvaOrange73@users.noreply.github.com>
2020-04-04 21:24:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-let x = 50;
-let y = 50;
 let v = 1;
-let alpha = 0.9;
+let alpha = 0;
 let ball = document.getElementById("ball");
-let dx = 0;
-let dy = 0;
-
 
 document.addEventListener('mousemove', moveBall, false);
 
 function moveBall(e){
-    ball.style.left = `${x}%`;
-    ball.style.top = `${y}%`;
-
-    let mx = e.clientX;
-    let my = e.clientY;
-    //console.log(`${mx} + ${my}`); 
-    let dx = mx - x;
-    let dy = my - y;
-    alpha = Math.atan(dy/dx);
 
     let bodyCoords = document.body.getBoundingClientRect();
     let ballCoords = ball.getBoundingClientRect();
-    if (ballCoords.right >= bodyCoords.right || x <= 0) {
+
+    let bx = ballCoords.lef;
+    let by = ballCoords.top;
+
+    let mx = e.clientX;
+    let my = e.clientY;
+
+    let dx = mx - bx - 50;
+    let dy = my - by - 50;
+
+    alpha = Math.atan(dy/dx);
+    if (dx < 0){
+        alpha = Math.PI + alpha;
+    }
+
+    if (ballCoords.right >= bodyCoords.right || ballCoords.left <= 0) {
         alpha = Math.PI - alpha;
-    } else if(ballCoords.bottom >= bodyCoords.bottom || y <= 0){
+    } else if(ballCoords.bottom >= bodyCoords.bottom || ballCoords.top <= 0){
         alpha = -alpha;
     }
 
-    x += v * Math.cos(alpha);
-    y += v * Math.sin(alpha);
+    bx += v * Math.cos(alpha);
+    by += v * Math.sin(alpha);
+
+    ball.style.left = `${bx}px`;
+    ball.style.top = `${by}px`;
+
 }
-
-
-
 
