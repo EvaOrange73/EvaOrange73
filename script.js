1ee1c56d15904be4af4685a35744923d43b1694e
EvaOrange73 <63116721+EvaOrange73@users.noreply.github.com>
2020-04-12 16:21:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let ball = document.getElementById("ball");
-
-document.addEventListener('mousemove', moveBall, false);
-
-function moveBall(e){
+function moveBall(){
 
     let bodyCoords = document.body.getBoundingClientRect();
     let ballCoords = ball.getBoundingClientRect();
@@ -10,22 +6,8 @@ function moveBall(e){
     let bx = ballCoords.left;
     let by = ballCoords.top;
 
-    let mx = e.clientX;
-    let my = e.clientY;
-
-    let dx = mx - bx - 50;
-    let dy = my - by - 50;
-
-    let alpha = Math.atan(dy/dx);
-    if (dx < 0){
-        alpha = Math.PI + alpha;
-    }
-
-    let R = Math.sqrt(dx**2 + dy**2)
-    let v = 3;
-    if (R <= 600){
-        v -= R / 200;
-    }
+    document.addEventListener('mousemove', changeAngle, false);
+    console.log(`${alpha}`)
 
     bx += v * Math.cos(alpha);
     by += v * Math.sin(alpha);
@@ -39,8 +21,36 @@ function moveBall(e){
     } else if (ballCoords.top <= 0){
         by = 1;
     }
+    
     ball.style.left = `${bx}px`;
     ball.style.top = `${by}px`;
 
 }
+
+function changeAngle(e) {
+    let ballCoords = ball.getBoundingClientRect();
+
+    let bx = ballCoords.left;
+    let by = ballCoords.top;
+
+    let mx = e.clientX;
+    let my = e.clientY;
+
+    let dx = mx - bx - 50;
+    let dy = my - by - 50;
+
+    alpha = Math.atan(dy/dx);
+    if (dx < 0){
+        alpha = Math.PI + alpha;
+    }
+
+    return alpha;
+
+}
+
+let ball = document.getElementById("ball");
+let alpha = 0;
+let v = 5;
+
+setInterval(moveBall, 40);
 
