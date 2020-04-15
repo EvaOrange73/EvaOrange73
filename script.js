5acfca1edbbd3bff118865f3e567f142c1ae89b7
EvaOrange73 <63116721+EvaOrange73@users.noreply.github.com>
2020-04-15 12:18:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function newBall(radius, color, name, x, y) {
     svg.setAttribute("style", "position:absolute");
     svg.setAttribute("id", name);
 
-    svg.style.left = x + "px";
-    svg.style.top = y + "px";
+    svg.style.transform = "translate(" + x + "px," + y + "px)";
 
     let cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     cir.setAttribute("cx", radius);
@@ -20,19 +19,17 @@ function newBall(radius, color, name, x, y) {
 };
 
 function params(i) {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    array[i][1] = '#' + r.toString(16) + g.toString(16) + b.toString(16); //color
+    littleBalls[i].name = String(i);
 
-    array[i][0] = 5 + Math.random() * 10; //radius
+    littleBalls[i].color = '#' + Math.floor(Math.random() * 256 * 256 * 256).toString(16)
 
-    array[i][5] = 1 + Math.random() * 5; //speed
-    array[i][6] = Math.random() * 2 * Math.PI //beta
+    littleBalls[i].radius = 5 + Math.random() * 10; //radius
 
-    let bodyCoords = document.body.getBoundingClientRect();
-    array[i][3] = Math.random() * bodyCoords.right; //x
-    array[i][4] = Math.random() * bodyCoords.bottom; //y
+    littleBalls[i].speed = 1 + Math.random() * 5; //speed
+    littleBalls[i].beta = Math.random() * 2 * Math.PI //beta
+
+    littleBalls[i].x = Math.random() * bodyCoords.right; //x
+    littleBalls[i].y = Math.random() * bodyCoords.bottom; //y
 
 }
 
@@ -40,15 +37,23 @@ function params(i) {
 function moveBall(name) {
     let ball = document.getElementById(name);
 
-    let bodyCoords = document.body.getBoundingClientRect();
     let ballCoords = ball.getBoundingClientRect();
 
     let bx = ballCoords.left;
     let by = ballCoords.top;
 
-    document.addEventListener('mousemove', function g(e) {
-        changeAngle(e, name)
+    document.addEventListener('mousemove', function (e) {
+        changeAlpha(e, name)
     }, false);
+
+    let R = Math.sqrt((mx - (bx + 50)) ** 2 + (my - (by + 50)) ** 2)
+    if (R > 100) {
+        v = V;
+    } else if (R > 40) {
+        v = R * V / 100;
+    } else {
+        v = 0;
+    }
 
     bx += v * Math.cos(alpha);
     by += v * Math.sin(alpha);
@@ -63,20 +68,19 @@ function moveBall(name) {
         by = 1;
     }
 
-    ball.style.left = `${bx}px`;
-    ball.style.top = `${by}px`;
+    ball.style.transform = "translate(" + bx + "px," + by + "px)";
 
 }
 
-function changeAngle(e, name) {
+function changeAlpha(e, name) {
     let ball = document.getElementById(name);
     let ballCoords = ball.getBoundingClientRect();
 
     let bx = ballCoords.left;
     let by = ballCoords.top;
 
-    let mx = e.clientX;
-    let my = e.clientY;
+    mx = e.clientX;
+    my = e.clientY;
 
     let dx = mx - bx - 50;
     let dy = my - by - 50;
@@ -91,71 +95,81 @@ function changeAngle(e, name) {
 }
 
 
-function moveSVG(array) {
-    let name = array[2];
+function moveLittleBalls(object, name, x, y, speed, beta, radius) {
     let svg = document.getElementById(name)
-    let x = array[3];
-    let y = array[4];
-    let beta = array[6];
-    let speed = array[5];
-    let radius = array[0];
-
-    let bodyCoords = document.body.getBoundingClientRect();
-    let svgCoords = svg.getBoundingClientRect();
 
     x += speed * Math.cos(beta);
     y += speed * Math.sin(beta);
 
-    if (svgCoords.right >= bodyCoords.right) {
-        x = bodyCoords.right - (2 * radius + 1);
-        array[6] = Math.PI - beta;
-    } else if (svgCoords.left <= 0) {
-        array[6] = Math.PI - beta;
-        x = 1;
-    } else if (svgCoords.bottom >= bodyCoords.bottom) {
-        y = bodyCoords.bottom - (2 * radius + 1);
-        array[6] = -beta;
-    } else if (svgCoords.top <= 0) {
-        y = 10;
-        array[6] = -beta;
+    if (x + 2 * radius >= bodyCoords.right) {
+        x = bodyCoords.right - (2 * radius);
+        object.beta = Math.PI - beta;
+    } else if (x <= 0) {
+        object.beta = Math.PI - beta;
+        x = 0;
+    } else if (y + 2 * radius >= bodyCoords.bottom) {
+        y = bodyCoords.bottom - (2 * radius);
+        object.beta = -beta;
+    } else if (y <= 0) {
+        y = 0;
+        object.beta = -beta;
     }
 
-    svg.style.left = `${x}px`;
-    svg.style.top = `${y}px`;
+    svg.style.transform = "translate(" + x + "px," + y + "px)";
 
-    array[3] = x;
-    array[4] = y;
+    object.x = x;
+    object.y = y;
+
 }
+
+
+let bodyCoords = document.body.getBoundingClientRect();
+window.addEventListener('resize', function () {
+    bodyCoords = document.body.getBoundingClientRect()
+}, false);
 
 //Движение большого шарика
 let mainBall = "mainBall";
 let alpha = 0;
-let v = 7;
+let V = 7;
 let BALL_X = 100;
 let BALL_Y = 100;
+let mx;
+let my;
 
 
 newBall(50, "black", mainBall, BALL_X, BALL_Y);
-setInterval(function fun() {
+setInterval(function () {
     moveBall(mainBall)
 }, 40);
 
 
 //Движение маленьких шариков
-let array = [];
-k = 239;
+let k = 1000;
 
+let littleBalls = [];
 for (let i = 0; i < k; i++) {
-    array[i] = []
-    array[i][2] = String(i);
+
+    littleBalls[i] = {};
+
     params(i);
 
-    newBall(array[i][0], array[i][1], array[i][2], array[i][3], array[i][4]);
+    newBall(littleBalls[i].radius,
+        littleBalls[i].color,
+        littleBalls[i].name,
+        littleBalls[i].x,
+        littleBalls[i].y);
 }
 
 
-setInterval(function f() {
+setInterval(function () {
     for (let i = 0; i < k; i++) {
-        moveSVG(array[i]);
+        moveLittleBalls(littleBalls[i],
+            littleBalls[i].name,
+            littleBalls[i].x,
+            littleBalls[i].y,
+            littleBalls[i].speed,
+            littleBalls[i].beta,
+            littleBalls[i].radius);
     }
 }, 40);
